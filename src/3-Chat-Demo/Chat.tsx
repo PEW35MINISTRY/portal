93f92affb60b9c7b97a4ab4d043a3bfef2da8723
Ethan Johnsrud <ethanjohnsrud@gmail.com>
2022-12-05 03:41:36 -0600

    [6] Circle Chat Working

--- src/3-Chat-Demo/Chat.tsx
+++ src/3-Chat-Demo/Chat.tsx
@@ -3,41 +3,63 @@ import { useAppSelector, useAppDispatch } from '../hooks';
 import { io, Socket } from "socket.io-client";
 import { Contact, SocketMessage } from './chat-types';
 import './chat.scss'; 
+import axios from 'axios';
 
 
 const Chat = () => {
     const JWT:string = useAppSelector((state) => state.account.JWT);
     const userId:number = useAppSelector((state) => state.account.userId);
 
-    const [email, setEmail] = useState<string>(''); 
-    const [password, setPassword] = useState<string>(''); 
-    const [socketId, setSocketId] = useState<number>(0); 
-
     const [circleMode, setCircleMode] = useState<boolean>(false); //local
     const [message, setMessage] = useState<string>(''); 
 
-    const [directChatSocket, setDirectChatSocket] = useState<Socket>();
+    const [chatSocket, setChatSocket] = useState<Socket>();
+
     const [contactList, setContactList] = useState<Contact[]>([]);
     const [contactId, setContactId] = useState<number>(0);
     const [directLog, setDirectLog] = useState<Array<string>>([]);
 
     const [circleList, setCircleList] = useState<Contact[]>([]);
     const [circleId, setCircleId] = useState<number>(0);
-    const [circleLog, setCircleLog] = useState<Array<string>>([]);
+    const [circleMapLog, setCircleMapLog] = useState<Map<number, Array<string>>>(new Map());
 
     
-    // const circleChat = io(`${process.env.REACT_APP_SOCKET_PATH}/circle`, {
-    //     path: '/chat/'
-    // });  
-
-    const formatEntry = (entry:string) => {
+    const formatEntry = (entry:string):string => {
         const time = new Date();
         return time.getMinutes() + ":" + time.getSeconds() + ' | ' + entry;
     }
 
     //Get list of all Contacts Individual
     useEffect(() => {
-        const directChatSocket = io(`${process.env.REACT_APP_SOCKET_PATH}`, {
+    /* Fetch Contacts */
+        axios.get(`${process.env.REACT_APP_DOMAIN}/api/contacts`, 
+            { headers: {
+                'user-id': userId,
+                'jwt': JWT
+            }
+        }).then(response => {
+            setContactList(response.data);
+            setContactId(response.data[0].id);
+            console.log('Contacts List ', response.data);
+            
+        }).catch(error => console.log('AXIOS Contacts Error:', error));
+
+    /* Fetch Circles */
+        axios.get(`${process.env.REACT_APP_DOMAIN}/api/circles`, 
+            { headers: {
+                'user-id': userId,
+                'jwt': JWT
+            }
+        }).then(response => {
+            setCircleList(response.data);
+            setCircleId(response.data[0].id);
+            console.log('Circles List ', response.data);
+            
+        }).catch(error => console.log('AXIOS Contacts Error:', error));
+
+
+    /* Socket Communication */
+        const socket = io(`${process.env.REACT_APP_SOCKET_PATH}`, {
             path: '/chat',
             auth: {
                 JWT: JWT,
@@ -45,70 +67,60 @@ const Chat = () => {
               }
         });  
 
-        setDirectChatSocket(directChatSocket);
+        setChatSocket(socket);
    
-        directChatSocket.on("contactMap", (list) => {
-            console.log('New Direct Contact Map:', list, JSON.parse(list)); 
-            setContactList(JSON.parse(list));
+        // socket.on("contactMap", (list) => {
+        //     console.log('New Direct Contact Map:', list, JSON.parse(list)); 
+        //     setContactList(JSON.parse(list));
+        //   });
+
+          socket.on("direct-message", (content: SocketMessage) => {
+            setDirectLog(old => [formatEntry(content.senderName?.toString() + ': ' + content.message), ...old]);
+            console.log('New Direct:', content); 
           });
 
-          directChatSocket.on("message", (context: SocketMessage) => {
-            setDirectLog(old => [formatEntry(context.senderName+': '+context.message), ...old]);
-            console.log('New Message:', context); 
+          socket.on("circle-message", (content: SocketMessage) => {
+            const circleId:number = content.recipientId;
+
+            setCircleMapLog(oldMap => {
+                const oldCircle:Array<string> = [formatEntry(content.senderName+': '+content.message), ...oldMap.get(circleId) || []];
+                oldMap.set(circleId, oldCircle);
+                console.log(oldMap, oldCircle, circleId);
+                return new Map(oldMap);
+            });            
+            console.log('New Circle:', content); 
           });
 
-          directChatSocket.on("server", (data: string) => {
+          socket.on("server", (data: string) => {
             setDirectLog(old => [formatEntry('Server: '+data), ...old]);
-            console.log('Server Message:', data); 
+            console.log('Server:', data); 
           });
 
-          directChatSocket.on("connection", (socket:Socket) => {
+          socket.on("connection", (socket:Socket) => {
             console.log('Direct Socket Connected:', socket.id); 
           });
 
-          directChatSocket.on("connect_error", (error) => { //https://socket.io/docs/v3/emitting-events/
+          socket.on("connect_error", (error) => { //https://socket.io/docs/v3/emitting-events/
             setDirectLog(old => [formatEntry('Error: '+error.message), ...old]);
             console.error(error.message); 
           });
 
           return () => { //Clean Up
-            directChatSocket.emit('leave', userId);
-            directChatSocket.disconnect();
+            socket.emit('leave', userId);
+            socket.disconnect();
          }
 
     },[]);
 
-    
-    //Get List of Circles and Ids
 
-    // circleChat.on("circleList", (response) => {
-    //     setCircleMap(response.data);
-    //     console.log('Circle List Updated:', response.data)
-    //   });
+    const sendMessage = (e:any) => {
+        e.preventDefault();
 
-
-    const onJoinServer = () => {
-        // directChat.on("connect", () => {
-        //     console.log('Direct Socket Connected', socket.id); // x8WIv7-mJelg7on_ALbx
-        //   });
-          
-        //   circleChat.on("connect", () => {
-        //     console.log('Circle Socket Connected', socket.id); // x8WIv7-mJelg7on_ALbx
-        //   });
-    }
-    
-    const onJoinCircle = () => {
-        // circleChat.on("connect", () => {
-        //     console.log('Circle Socket Connected', socket.id); // x8WIv7-mJelg7on_ALbx
-        //   });
-    }
-
-    const sendMessage = () => {
-        directChatSocket?.emit("message", {
+        chatSocket?.emit(circleMode ? "circle-message" : "direct-message", {
             senderId: userId,
-            recipientId: contactId,
+            recipientId: circleMode ? circleId : contactId,
             message: message
-        });        
+        });     
     }
     
 
@@ -127,7 +139,7 @@ const Chat = () => {
 
                     <section id='contact-select'>
                         <label >Select Contact:</label>
-                        <select name="contact" id="contact-select" placeholder='Select Online Contact'>
+                        <select name="contact" id="contact-select" placeholder='Select Online Contact' defaultValue={(circleMode ? circleId : contactId)} >
                             {(circleMode ? circleList : contactList).map((contact, i) => 
                                 <option key={i} className="entry" onClick={()=>(circleMode ? setCircleId(contact.id) : setContactId(contact.id))} value={contact.id}>{contact.name}</option>
                             )}
@@ -137,13 +149,13 @@ const Chat = () => {
                             onClick={onJoinCircle}>Join Circle</button> */}
                     </section>
 
-                    <textarea onChange={(e)=>setMessage(e.target.value)} value={message} onKeyDown={(e)=>{if(e.key === 'Enter') {e.preventDefault(); sendMessage();}}} placeholder='Enter Message Here...'></textarea>
+                    <textarea onChange={(e)=>setMessage(e.target.value)} value={message} onKeyDown={(e)=>{if(e.key === 'Enter') {sendMessage(e);}}} placeholder='Enter Message Here...'></textarea>
                     <button onClick={sendMessage} >Send Message</button>
                 </div>
             
                 <div id="chat-log">
                     <label >Chat History:</label>
-                    {(circleMode ? circleLog : directLog).map((item, i) => 
+                    {(circleMode ? (circleMapLog.get(circleId) || []) : directLog).map((item, i) => 
                         <p key={i} className="entry">{item}</p>
                     )}
                 </div>
