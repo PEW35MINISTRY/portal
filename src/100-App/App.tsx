6196421b1fb1504593209cd2de307c9a141baa76
Ethan Johnsrud <ethanjohnsrud@gmail.com>
2023-10-29 22:24:41 -0500

    64-Navigation-Redesign  Moved AppContent to seperate file and config list for easily adding new pages

--- src/100-App/App.tsx
+++ src/100-App/App.tsx
@@ -4,20 +4,17 @@ import { BrowserRouter, Link, NavLink, Navigate, Route, Routes, useLocation, use
 import { Slide, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useAppDispatch, useAppSelector } from '../1-Utilities/hooks';
-import './App.scss';
 import store, { AppDispatch, logoutAccount } from './redux-store';
 
+import './App.scss';
 
 //Components
 import Login from '../11-Models/Login';
-import UserEditPage from '../11-Models/UserEditPage';
-import UserSignUpPage from '../11-Models/UserSignUpPage';
-import CircleChat from '../12-Features/Chat-Circle-Demo/Chat';
-import DirectChat from '../12-Features/Chat-Direct-Demo/Chat';
-import Log from '../12-Features/Log';
+import SignUpPage from '../11-Models/UserSignUpPage';
+import AppContent from './AppContent';
 
 //Assets
-import LOGO from '../0-Assets/logo.png';
+import LOGO from './0-Assets/logo.png';
 
 
 const App = () => {
@@ -25,7 +22,11 @@ const App = () => {
   return (
     <BrowserRouter>
         <Provider store={store}>
-        <AppContent/>
+        <Routes>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/signup' element={<SignUpPage/>}/>
+            <Route path='/*' element={<AppContent/>}/>
+        </Routes>
         <ToastContainer
             position='top-right'
             autoClose={5000}
@@ -43,88 +44,3 @@ const App = () => {
 }
 
 export default App;
-
-const AppContent = () => {
-  const dispatch = useAppDispatch();
-
-  const displayName:string = useAppSelector((state: { account: { userProfile: { displayName: any; }; }; }) => state.account.userProfile.displayName);
-  const userID:number = useAppSelector((state: { account: { userID: any; }; }) => state.account.userID);
-  const jwt:string = useAppSelector((state: { account: { jwt: any; }; }) => state.account.jwt);
-
-  return (
-      <div id='app'>
-
-        <div id='app-navigation'>
-          <Link to='/portal/dashboard' style={{ textDecoration: 'none' }}>
-            <div id='logo-box' >
-              <img src={LOGO} alt='log-title'/>
-              <h1>Encouraging Prayer</h1>
-            </div>
-          </Link>
-
-          <div id='page-menu'>
-            <NavLink to='/login'  className={({ isActive }) => (isActive ? 'active' : '')+' page'}>
-              + Login
-            </NavLink>
-            <NavLink to='/signup' className={({ isActive }) => (isActive ? 'active' : '')+' page'}>
-              &#9840; Sign Up
-            </NavLink>
-            <NavLink to={`/portal/edit/profile/${userID}`} className={({ isActive }) => (isActive ? 'active' : '')+' page'}>
-              &#9840; Edit Profile
-            </NavLink>
-            <NavLink to='/portal/chat/direct' className={({ isActive }) => (isActive ? 'active' : '')+' page'}>
-              &#9840; Direct Chat
-            </NavLink>
-            <NavLink to='/portal/chat/circle' className={({ isActive }) => (isActive ? 'active' : '')+' page'}>
-              &#9840; Circle Chat
-            </NavLink>
-            <NavLink to='/portal/logs' className={({ isActive }) => (isActive ? 'active' : '')+' page'}>
-              &#9840; Logs
-            </NavLink>
-
-            <button className={'page'} onClick={()=>store.dispatch((logoutAccount) as AppDispatch)}>
-              - Logout
-            </button>
-          </div>
-          
-        </div>
-
-        <div id='app-content'>
-          <div id='app-header'>
-              <h2>Welcome to the Portal</h2>
-              <NavLink to='/login' id='profile-box'>
-                <label >{displayName} | {userID} </label>
-              </NavLink>
-          </div>
-
-          <div id='app-body'>
-          {/* Priority Order */}
-              <Routes>
-                <Route path='/portal' element={ <Navigate to='/portal/dashboard' /> }/>
-                <Route path='/portal/dashboard' element={'Dashboard coming Eventually :)'}/>
-                <Route path='/login' element={<Login/>}/>
-                <Route path='/signup' element={<UserSignUpPage/>}/>
-                <Route path='/portal/edit/profile/:id' element={<UserEditPage/>}/>
-                <Route path='/portal/chat/direct' element={<DirectChat/>}/>
-                <Route path='/portal/chat/circle' element={<CircleChat/>}/>
-                <Route path='/portal/logs' element={<Log/>}/>
-                <Route path='*' element={<PageNotFound/>} />
-              </Routes>
-
-          </div>
-        </div>
-        
-      </div>
-  );
-}
-
-const PageNotFound = () => {
-
-  return (
-    <div id='page-not-found'>
-      <h3>Sorry page not Found</h3>
-      <Link to='/portal'>
-        <button >Return to Dashboard</button>
-      </Link>
-    </div>);
-}
---
+++ src/100-App/AppContent.tsx
@@ -0,0 +1,165 @@
+import { useRef, useState } from 'react';
+import { Link, NavLink, Navigate, Route, Routes, useNavigate } from 'react-router-dom';
+import { RoleEnum } from '../0-Assets/field-sync/input-config-sync/profile-field-config';
+import { useAppDispatch, useAppSelector } from '../1-Utilities/hooks';
+import { makeDisplayText } from '../1-Utilities/utilities';
+import store, { AppDispatch, logoutAccount } from './redux-store';
+
+import './App.scss';
+
+//Components
+import Dashboard from '../10-Dashboard/Dashboard';
+import UserEditPage from '../11-Models/UserEditPage';
+import CircleChat from '../12-Features/Chat-Circle-Demo/Chat';
+import DirectChat from '../12-Features/Chat-Direct-Demo/Chat';
+import Log from '../12-Features/Log';
+
+//Assets
+import LOGO from '../0-Assets/logo.png';
+import PROFILE_ICON from '../0-Assets/profile-icon-blue.png';
+import PROFILE_ICON_ACTIVE from '../0-Assets/profile-icon-white.png';
+import CIRCLE_ICON from '../0-Assets/circle-icon-blue.png';
+import CIRCLE_ICON_ACTIVE from '../0-Assets/circle-icon-white.png';
+import PRAYER_REQUEST_ICON from '../0-Assets/prayer-request-icon-blue.png';
+import PRAYER_REQUEST_ICON_ACTIVE from '../0-Assets/prayer-request-icon-white.png';
+import DIRECT_CHAT_ICON from '../0-Assets/messaging-icon-blue.png';
+import DIRECT_CHAT_ICON_ACTIVE from '../0-Assets/messaging-icon-white.png';
+import CIRCLE_CHAT_ICON from '../0-Assets/chat-icon-blue.png';
+import CIRCLE_CHAT_ICON_ACTIVE from '../0-Assets/chat-icon-white.png';
+import LOG_ICON from '../0-Assets/log-icon-blue.png';
+import LOG_ICON_ACTIVE from '../0-Assets/log-icon-white.png';
+import PREFERENCES_ICON from '../0-Assets/settings-icon-blue.png';
+import PREFERENCES_ICON_ACTIVE from '../0-Assets/settings-icon-white.png';
+import LOGOUT_ICON from '../0-Assets/logout-icon-blue.png';
+import LOGOUT_ICON_ACTIVE from '../0-Assets/logout-icon-white.png';
+
+
+type MenuPageListing = {
+  label:string,
+  route?:string,
+  onClick?:Function,
+  activeIcon:string,
+  inactiveIcon:string,
+  addRoute?:string,
+  exclusiveRoleList?:RoleEnum[] //Roles to show only; undefined allows all
+}
+
+
+const AppContent = () => {
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+    const userID:number = useAppSelector((state) => state.account.userID);
+    const userRole:string = useAppSelector((state) => state.account.userProfile.userRole);
+    const displayName:string = useAppSelector((state) => state.account.userProfile.displayName);
+    const profileImage:string|undefined = useAppSelector((state) => state.account.userProfile.image);
+
+    const menuRef = useRef<null | HTMLDivElement>(null);
+    const [showMenu, setShowMenu] = useState<boolean>(true);
+    const [showProfileMenu, setShowProfileMenu] = useState<boolean>(false);
+
+    const MENU_CONFIG_LIST:MenuPageListing[] = [
+      {label: 'Profile', route: `/portal/edit/profile/${userID}`, activeIcon: PROFILE_ICON_ACTIVE, inactiveIcon: PROFILE_ICON},
+      {label: 'Messages', route: '/portal/chat/direct', activeIcon: DIRECT_CHAT_ICON_ACTIVE, inactiveIcon: DIRECT_CHAT_ICON},
+      {label: 'Circle Chat', route: '/portal/chat/circle', activeIcon: CIRCLE_CHAT_ICON_ACTIVE, inactiveIcon: CIRCLE_CHAT_ICON},
+      {label: 'Logs', route: '/portal/logs', activeIcon: LOG_ICON_ACTIVE, inactiveIcon: LOG_ICON, exclusiveRoleList: [RoleEnum.ADMIN]},
+    ];
+
+    const PROFILE_MENU_CONFIG_LIST:MenuPageListing[] = [
+      {label: 'Preferences', activeIcon: PREFERENCES_ICON_ACTIVE, inactiveIcon: PREFERENCES_ICON},
+      {label: 'Logout', route: '/login', onClick: ()=>store.dispatch((logoutAccount) as AppDispatch), activeIcon: LOGOUT_ICON_ACTIVE, inactiveIcon: LOGOUT_ICON},
+    ];
+
+    return (
+        <div id='app'>
+  
+          <div id='app-navigation' className={showMenu ? '' : 'collapse'} ref={menuRef} onClick={(event)=>{if(event.currentTarget === event.target) setShowMenu(current => !current)}}>
+            <Link to='/portal/dashboard' style={{ textDecoration: 'none' }}>
+              <div id='logo-box' className='page' >
+                <img src={LOGO} alt='log-title'/>
+                <h1 className={showMenu ? '' : 'hide'} >Encouraging Prayer</h1>
+              </div>
+            </Link>
+
+            <button id='menu-close-button' className='hide' onClick={()=>setShowMenu(current => !current)} ><p className={showMenu ? 'less-than' : 'greater-than'}>{showMenu ? '<' : '>'}</p></button>
+
+            <div id='app-menu'>
+
+              {MENU_CONFIG_LIST.map((page, index) => 
+                    <NavLink key={'menu-'+index} to={page.route || ''} onClick={()=> page.onClick && page.onClick()}
+                        className={({ isActive }) => (isActive ? 'active' : '') + ' page' + ((page.exclusiveRoleList === undefined || page.exclusiveRoleList.includes(userRole as RoleEnum)) ? '' : ' hide')}>
+                      {({ isActive }) => (
+                        <>
+                          <img className={isActive ? 'active-icon' : 'active-icon hide'} src={page.activeIcon} />
+                          <img className={isActive ? 'inactive-icon hide' : 'inactive-icon'} src={page.inactiveIcon} />
+                          <label className={showMenu ? '' : 'hide'} >{page.label}</label>
+                          {page.addRoute && showMenu &&
+                            <section className='add-button-wrapper' onClick={(event)=> {event.preventDefault(); event.stopPropagation(); navigate(page.addRoute || 'menu/add/'+index);}} >
+                              <span className='add-button' ><p>+</p></span>
+                            </section>}
+                        </>
+                      )}
+                    </NavLink>
+                )}
+
+            </div>
+
+            {showProfileMenu && <div id='absolute-wrapper' onClick={()=>setShowProfileMenu(false)}></div> }
+
+            {showProfileMenu &&
+              <div id='profile-menu' style={{minWidth:menuRef.current?.offsetWidth || 'auto'}} >
+
+                {PROFILE_MENU_CONFIG_LIST.map((page, index) => 
+                  <NavLink key={'profile-menu-'+index} to={page.route || 'menu/profile/'+index}  onClick={() => {setShowProfileMenu(false); if(page.onClick) page.onClick();}}
+                      className={'page' + ((page.exclusiveRoleList === undefined || page.exclusiveRoleList.includes(userRole as RoleEnum)) ? '' : ' hide')}>
+                        <img className={'active-icon'} src={page.activeIcon} />
+                        <img className={'inactive-icon'} src={page.inactiveIcon} />
+                        <label className={showMenu ? '' : 'hide'} >{page.label}</label>
+                        {page.addRoute && showMenu &&
+                            <section className='add-button-wrapper' onClick={(event)=> {event.preventDefault(); event.stopPropagation(); navigate(page.addRoute || 'menu/profile/add'+index);}} >
+                              <span className='add-button' ><p>+</p></span>
+                            </section>}
+                  </NavLink>
+                )}
+                
+              </div> }
+
+            <section id='profile-box' className={'page'} onClick={()=>setShowProfileMenu(true)}>             
+              <img className={'active-icon'} src={profileImage || PROFILE_ICON_ACTIVE} alt='User-Profile-Image' />
+              <img className={'inactive-icon'} src={profileImage || PROFILE_ICON} alt='User-Profile-Image' />
+              <section id='profile-box-vertical' className={showMenu ? '' : 'hide'} >
+                <h2 className={showMenu ? '' : 'hide'} >{displayName}</h2>
+                <h5 className={showMenu ? '' : 'hide'} >{makeDisplayText(userRole)}</h5>
+              </section>
+            </section>            
+          </div>
+  
+          <div id='app-content'>
+               {/* Priority Order */}
+                <Routes>
+                  <Route path='/portal' element={ <Navigate to='/portal/dashboard' /> }/>
+                  <Route path='/portal/dashboard/*' element={<Dashboard/>}/>
+                  <Route path='/portal/edit/profile/:id/*' element={<UserEditPage/>}/>
+                  <Route path='/portal/chat/direct/*' element={<DirectChat/>}/>
+                  <Route path='/portal/chat/circle/*' element={<CircleChat/>}/>
+                  <Route path='/portal/logs/*' element={<Log/>}/>
+                  <Route path='*' element={<PageNotFound/>} />
+                </Routes>
+            </div>
+        </div>
+    );
+  }
+
+  export default AppContent;
+  
+  const PageNotFound = () => {
+  
+    return (
+      <div id='page-not-found'>
+        <div>
+          <h1>Sorry Page Not Found</h1>
+          <Link to='/portal'>
+            <button >Return to Dashboard</button>
+          </Link>
+        </div>
+      </div>);
+  }
